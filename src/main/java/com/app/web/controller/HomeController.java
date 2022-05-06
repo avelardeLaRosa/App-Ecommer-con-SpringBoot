0f1c2483cdca4ee51475f60bc9d40db27589eb59
DESKTOP-9V0MFI3\hp <vlex94@hotmail.com>
2022-05-06 11:51:13 -0500

    se agrego la data de un producto al presionar ver producto

--- src/main/java/com/app/web/controller/HomeController.java
+++ src/main/java/com/app/web/controller/HomeController.java
@@ -1,24 +1,45 @@
 package com.app.web.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.web.model.Producto;
 import com.app.web.service.ProductoService;
 
 @Controller
 @RequestMapping("/") //este controlador mapeare la raiz del proyecto
 public class HomeController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
 	@Autowired
 	private ProductoService productoService;
 	
 	@GetMapping("") //este metodo llama a la raiz
-	public String homre(Model model) {
+	public String home(Model model) {
 		model.addAttribute("productos",productoService.findAll());
 		return "usuario/home";
 	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		LOGGER.info("Id producto enviado {}",id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto",producto);
+		return "usuario/productohome";
+	}
+	
+	
 
 }
