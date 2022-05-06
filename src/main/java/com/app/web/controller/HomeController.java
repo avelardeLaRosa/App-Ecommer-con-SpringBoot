37fdb53e55d01472ae54b0f28e56116dcf0ebeb5
DESKTOP-9V0MFI3\hp <vlex94@hotmail.com>
2022-05-06 12:18:00 -0500

    se agrego poder añadir al carrito un producto

--- src/main/java/com/app/web/controller/HomeController.java
+++ src/main/java/com/app/web/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.app.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -9,8 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.app.web.model.DetalleOrden;
+import com.app.web.model.Orden;
 import com.app.web.model.Producto;
 import com.app.web.service.ProductoService;
 
@@ -22,6 +28,11 @@ public class HomeController {
 
 	@Autowired
 	private ProductoService productoService;
+	
+	//para almacenar los detalles de la orden
+	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+	//el obj almacenara los datos de la orden
+	Orden orden = new Orden();
 	
 	@GetMapping("") //este metodo llama a la raiz
 	public String home(Model model) {
@@ -38,6 +49,21 @@ public class HomeController {
 		
 		model.addAttribute("producto",producto);
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		//parametros : id y la cantidad
+		
+		DetalleOrden detalleOrden = new DetalleOrden();
+		Producto producto=new Producto();
+		double sumaTotal=0; //suma de productos agregados
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		LOGGER.info("el producto añadido: {}" , optionalProducto.get());
+		LOGGER.info("cantidad {}" , cantidad);
+		
+		return "usuario/carrito";
 	}
 	
 	
